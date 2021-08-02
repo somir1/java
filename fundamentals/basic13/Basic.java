6f4f44d46e14321a534faa9252d13baac2b9e297
somir1 <leonsomir@gmail.com>
2021-08-02 19:18:57 -0400

    basic13

---
+++ fundamentals/basic13/Basic.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Basic{
+
+    public static void main(String[] args) {
+        
+        printNum();
+        oddNum();
+        System.out.println(sum(2));
+        int[] num = {5,6,8,7,9,1,2000};
+        ittArray(num);
+    }
+
+    public static void printNum(){
+        for (int i = 1; i < 256; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void oddNum(){
+        for (int i = 1; i < 256; i++){
+            if (i % 2 != 0 ){
+            System.out.println(i);
+            }
+        }
+    }
+
+    public static int sum (int num){
+        int total = 0;
+        for (int i = 1; i <= num; i++){
+            total += i;
+        }
+        return total;
+    }
+
+    public static void ittArray(int[] inputArr){
+        for (int value: inputArr){
+        System.out.println(value);
+        }
+    }
+
+    public static void findMax(int[] inputArr){
+        int maxVal = inputArr[0];
+        for (int value: inputArr){
+            if (value > maxVal){
+                maxVal = value;
+            }
+        }
+        System.out.println(maxVal);
+    }
+
+    public static ArrayList<Integer> getOdd(int someNum) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i <= 255; i++){
+            if (i % 2 != 0){
+                result.add(i);
+            }
+            
+        }
+        System.out.println(result);
+        return result;
+    }
+}
